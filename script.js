3e3e3f3f93cec2fb3d1662e38c17da2c3c6ba7de
Demicolon <94819198+TheDemicolon@users.noreply.github.com>
2025-01-13 14:33:37 -0500

    added screenshots to experience

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const dynamicText = document.querySelector("h1 span");
-const words = ["Charlie Zhang", "a Game Developer", "a 3D Artist", "a Student"];
+const words = ["Charlie Zhang", "a Game Developer", "章乐易", "a 3D Artist", "a Student", "a Programmer"];
 
 let wordIndex = 0;
 let charIndex = 0;
@@ -11,16 +11,16 @@ const typeEffect = () => {
     if (!isDeleting && charIndex < currentWord.length) {
 
         charIndex++;
-        setTimeout(typeEffect, 200);
+        setTimeout(typeEffect, 150);
     } else if (isDeleting && charIndex > 0) {
         // If condition is true, remove the previous character
         charIndex--;
-        setTimeout(typeEffect, 100);
+        setTimeout(typeEffect, 75);
     } else {
         // If word is deleted then switch to the next word
         isDeleting = !isDeleting;
         wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200);
+        setTimeout(typeEffect, 300);
     }
 }
 typeEffect();
